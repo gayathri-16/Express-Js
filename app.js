3cf45fe694e2788c84ed4cfae071042a2b34e8fa
gayathri-16 <74528512+gayathri-16@users.noreply.github.com>
2023-02-14 06:48:37 +0530

    Update app.js
    using Path Module in express.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 
 const express = require('express')
 const app = express();
+const path = require('path')
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
+
+app.use(express.static(path.join(__dirname,'public')))
+
 app.use(bodyParser.urlencoded())
-app.use('/admin', adminRoutes)
+app.use(adminRoutes)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
